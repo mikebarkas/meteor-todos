69b4df0339e2d8d3bb968b7c3a89ce2ac7894b9b
Mike Barkas <mike@barkas.com>
2015-04-30 21:59:52 -0400

    Todo item event handler and template helper.

--- app/client/templates/todos/todos_item/todos_item.js
+++ app/client/templates/todos/todos_item/todos_item.js
@@ -2,12 +2,24 @@
 /* TodosItem: Event Handlers */
 /*****************************************************************************/
 Template.TodosItem.events({
+  'click [name=isDone]': function (e, tmpl) {
+    var id = this._id;
+    var isDone = tmpl.find('input').checked;
+    Todos.update({_id: id}, {
+      $set: {
+        isDone: isDone
+      }
+    });
+  }
 });
 
 /*****************************************************************************/
 /* TodosItem: Helpers */
 /*****************************************************************************/
 Template.TodosItem.helpers({
+  isDoneChecked: function () {
+    return this.isDone ? 'checked' : '';
+  }
 });
 
 /*****************************************************************************/
